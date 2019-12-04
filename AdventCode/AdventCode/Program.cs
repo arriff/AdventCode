4db0d5c7807aa6d9aa6b54b0f5a2df45dc9ca01c
Mohamed a <m-adda@hotmail.nl>
2019-12-05 00:23:44 +0100

    Day_02_1

---
+++ AdventCode/AdventCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Day_02_1();
+            x.Run();
+        }
+    }
+}
